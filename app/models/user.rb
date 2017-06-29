1144b9cc6fd25197916c9aa09361b57b5f802653
oga3999 <zgmf.x09a3999@gmail.com>
2017-06-29 15:07:35 +0000

    dive_14_sns_login_second

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,:confirmable,:omniauthable
-  mount_uploader :avater, AvaterUploader
-  has_many :blogs
+         
+   mount_uploader :avatar, AvatarUploader
+   has_many :blogs
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.find_by(email: auth.info.email)
