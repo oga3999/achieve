93e0c15d5a263213e57cb0313e93e02cf8dde23a
oga3999 <zgmf.x09a3999@gmail.com>
2017-07-22 03:23:45 +0000

    dive-16-task

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   devise_for :users, controllers:{
     registrations: "users/registrations",
     omniauth_callbacks:"users/omniauth_callbacks"
   }
+
+  resources :users, only:[:index, :show]
+
+  resources :relationships, only: [:create, :destroy]
 
   resources :blogs do
     post :confirm, on: :collection
