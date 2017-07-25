1bfb45ad3905d7339f59bc1e006f044d9547eb2d
oga3999 <zgmf.x09a3999@gmail.com>
2017-07-25 21:47:40 +0000

    dive-19-2-task

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'notifications/index'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   devise_for :users, controllers:{
@@ -29,6 +31,10 @@ Rails.application.routes.draw do
   end
 
   resources :poems, only:[:index,:show]
+
+  resources :conversations do
+    resources :messages
+  end
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
